002683342a2e0965cb82aa9fb449ac2004894063
Anupam <89902084+Anupamk06@users.noreply.github.com>
2022-02-10 06:51:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+import random
+images=[rock,paper,scissors]
+user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+if user_choice>=3 and user_choice<0:  #invalid choice 
+  print("invalid choice")
+else :
+  print(images[user_choice])
+
+computer_choice=random.randint(0,2)   #random integer from 0 to 2
+print("computer chooses")
+print(images[computer_choice])
+
+if user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
